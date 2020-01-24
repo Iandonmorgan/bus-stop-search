4688c180a16ffb821b9c999ac0f19e4c1023b44c
Landon Morgan <morganlandon@gmail.com>
2020-01-24 11:25:11 -0600

    Bus Stop Search practice completed, initial commit with working search and display

---
+++ src/scripts/searchResultsDomManager.js
@@ -0,0 +1,17 @@
+const searchResultsDomManager = {
+    busStopFactory(busStop) {
+        return `
+            <section class="bus-stop">
+                <div class="bus-stop__route-name">${busStop.route_name}</div>
+                <div class="bus-stop__stopname">${busStop.stopname}</div>
+            </section>
+        `
+    },
+    renderSearchResults(searchResults) {
+        const container = document.querySelector("#search-results");
+        container.innerHTML = "";
+        searchResults.forEach(busStop => {
+            container.innerHTML += this.busStopFactory(busStop)
+        })
+    }
+};
